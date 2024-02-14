18b67d60ea95acf4e1431afeab3bb3306d3298e0
Abhay-Riyal <abhay.7riyal@gmail.com>
2024-02-14 19:30:55 +0530

    added middleware and updated files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() =>{
@@ -13,9 +14,20 @@ mongoose.connect(process.env.MONGO).then(() =>{
 const app=express();
 const PORT=3000;
 
-
+app.use(express.json());
 app.listen(PORT,()=>{
     console.log(`server is listening on port ${PORT}`);
 })
 
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+
+app.use((err, req, res, next)=>{
+    const statusCode=err.statusCode || 500;
+    const message=err.message || 'Internal Server Error';
+    return res.staus(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+})
