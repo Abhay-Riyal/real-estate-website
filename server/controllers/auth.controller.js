090ca8b2683c0407011453a36a0d283ff158df9d
omkar930 <93o69644o9@gmail.com>
2024-02-18 17:06:57 +0530

    solved some bugs and created the sign in page

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req,res,next) =>{
     const {username, email, password} = req.body;
@@ -24,7 +25,7 @@ export const signin = async(req,res,next) => {
         const validPassword = bcryptjs.compareSync(password,validUser.password);
         if(!validPassword) return next(errorHandler(401,"wrong credentials"));
         const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-        const {password: pass ...rest} = validUser._doc
+        const {password: pass, ...rest} = validUser._doc;
         res
         .cookie('access_token',token,{httpOnly: true})
         .status(200)
@@ -32,4 +33,4 @@ export const signin = async(req,res,next) => {
     }catch(error){
         next(error)
     }
-}
+};
