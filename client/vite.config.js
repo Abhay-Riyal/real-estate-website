bceb911e4e5fc5ca0217dff5f4274bf91c0c9982
omkar930 <93o69644o9@gmail.com>
2024-03-27 23:02:24 +0530

    ...

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    // proxy: {
+    //   '/api' : {
+    //    // target: 'http://localhost:3000',
+    //    target: 'https://real-estate-website-server-blue.vercel.app',
+    //     secure: false,
+    //   },
+    // },
     proxy: {
-      '/api' : {
-       // target: 'http://localhost:3000',
-       target: 'http://real-estate-website-server-blue.vercel.app',
-        secure: false,
-      },
-    },
+      '/api': {
+           target: 'https://real-estate-website-server-blue.vercel.app',
+           changeOrigin: true,
+           secure: false,      
+           ws: true,
+       }
+  }
   },
   plugins: [react()],
 });
