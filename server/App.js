b9202eb27f5567d1b07305080e88c55ca1cc883d
omkar930 <93o69644o9@gmail.com>
2024-03-27 23:22:52 +0530

    ..

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import cors from cors
 
 const App = express();
 App.use(express.json());
@@ -10,4 +11,11 @@ App.use(cookieParser());
 App.use('/api/user', userRouter);
 App.use('/api/auth', authRouter);
 App.use('/api/listing', listingRouter);
+
+App.use(cors({
+    origin: {"https://real-estate-website-pink.vercel.app/"},
+    methods: ["POST","GET"],
+}
+    
+))
 export default App;
