ad53cda0be201624cd85974bb4908c91b9257460
omkar930 <93o69644o9@gmail.com>
2024-03-27 23:48:21 +0530

    ..

--- server/App.js
+++ server/App.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
+import cors from cors
 
 const App = express();
 App.use(express.json());
@@ -11,5 +12,11 @@ App.use('/api/user', userRouter);
 App.use('/api/auth', authRouter);
 App.use('/api/listing', listingRouter);
 
-
+App.use(cors({
+    origin: {"https://real-estate-website-client-seven.vercel.app"},
+    methods: ["POST","GET"],
+  //  credentials: true,
+}
+    
+))
 export default App;
