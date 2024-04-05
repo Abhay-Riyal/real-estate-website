a6e57b9a18826bcbca20c43cf879c02d2842b2f4
omkar930 <93o69644o9@gmail.com>
2024-04-05 19:00:04 +0530

    ...

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://real-estate-website-bfnf.vercel.app',
+        target: 'https://real-estate-website-server-henna.vercel.app',
         secure: true,
         changeOrigin: true,
       },
